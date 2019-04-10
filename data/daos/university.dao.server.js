6830b843bc6b006a86088c72408e304e81fa87f4
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2019-04-09 20:17:13 -0400

    Daos ready

--- data/daos/university.dao.server.js
+++ data/daos/university.dao.server.js
@@ -1,9 +1,49 @@
 const studentModel = require('../models/student.model.server')
+const questionModel = require('../models/question.model.server')
+const answerModel = require('../models/answer.modal.server')
+
+module.exports = {
+    createStudent,
+    findAllAnswers,
+    findAllStudents,
+    findAnswerById,
+    findStudentById,
+    findAllQuestions,
+    findQuestionById,
+    findAnswersByStudent,
+    findAnswersByQuestion  
+  }
 
 const createStudent = student =>
   studentModel.create(student)
 
+const findAllStudents = () => {
+    studentModel.find();
+}  
 
-module.exports = {
-  createStudent
+const findStudentById = userId =>
+  studentModel.findById(userId)
+
+const findAllQuestions = () =>{
+      questionModel.find();
 }
+
+const findQuestionById = id => {
+    questionModel.findById(id)
+}
+
+const findAllAnswers = () =>{
+    answerModel.find();
+}
+const findAnswerById = id => {
+    answerModel.findById(id);
+} 
+  
+const findAnswersByStudent = (studentId) => {
+    answerModel.findAnswersByStudent(studentId)
+} 
+const findAnswersByQuestion = (questionId) =>{
+    questionModel.findAnswersByQuestion(questionId);  
+} 
+  
+
--- services/university.services.server.js
+++ services/university.services.server.js
@@ -1,7 +1,11 @@
 const universityDao = require('../data/daos/university.dao.server')
 module.exports = app => {
-    createStudent = (req, res) =>
+    const createStudent = (req, res) =>
     res.json(universityDao.createStudent(req.body))
 
+    const findAllStudents = (req, res) =>
+    res.json(universityDao.findStudents())
+
   app.post('/api/student/', createStudent)
+  app.get('/api/student/', findAllStudents)
 }
